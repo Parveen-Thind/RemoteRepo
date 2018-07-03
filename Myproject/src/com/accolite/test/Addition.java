1a36eaf18a0fdf82f274773c5e8d5193df1eefc7
parveen <parveen@ACCO-DEL-01>
2018-07-03 16:16:23 +0530

    first commit

---
+++ Myproject/src/com/accolite/test/Addition.java
@@ -0,0 +1,14 @@
+package com.accolite.test;
+
+public class Addition {
+	public int sum(int[] a)
+	{
+		int sum = 0;
+		for(int i =0;i<3;i++)
+		{
+			sum += a[i];
+		}
+		return sum;
+	}
+
+}
---
+++ Myproject/src/com/accolite/test/Mymain.java
@@ -0,0 +1,11 @@
+package com.accolite.test;
+
+public class Mymain {
+	public static void main(String[] args)
+	{
+		Addition a = new Addition();
+		
+		System.out.println("sum = "+a.sum(new int[] {1 , 2, 3} ));
+	}
+
+}
